5b50cc8f8b7b150b9ebf24acf3f8fd9bde8fa8fc
Tom ANDRE <tom.andre@soprasteria.com>
2024-10-24 08:38:43 +0200

    first commit

---
+++ src/main/java/com/chatop/p3chatop/P3ChatopApplication.java
@@ -0,0 +1,13 @@
+package com.chatop.p3chatop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ChatopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P3ChatopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chatop/p3chatop/P3ChatopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatop.p3chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ChatopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
