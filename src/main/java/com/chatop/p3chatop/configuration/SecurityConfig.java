c6f3b50e88f2337581e95de81641442665c37b36
Tom ANDRE <tom.andre@soprasteria.com>
2024-10-26 20:15:51 +0200

    feat(STEP-4): Ajout de la documentation swagger

--- src/main/java/com/chatop/p3chatop/configuration/SecurityConfig.java
+++ src/main/java/com/chatop/p3chatop/configuration/SecurityConfig.java
@@ -33,6 +33,8 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
         );
         http.sessionManagement(session -> session
--- src/main/java/com/chatop/p3chatop/controllers/AuthenticationController.java
+++ src/main/java/com/chatop/p3chatop/controllers/AuthenticationController.java
@@ -7,11 +7,22 @@ import com.chatop.p3chatop.dto.TokenResponseDTO;
 import com.chatop.p3chatop.entities.User;
 import com.chatop.p3chatop.repositories.UserRepository;
 import com.chatop.p3chatop.services.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Authentification", description = "Authentification management APIs")
 @RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
@@ -24,6 +35,14 @@ public class AuthenticationController {
         this.userRepository = userRepository;
     }
 
+
+    @Operation(
+            summary = "Authentification d'un utilisateur",
+            description = "Authentifie un utilisateur avec les informations fournies."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TokenResponseDTO.class), mediaType = "application/json") }),
+    })
     @PostMapping("/register")
     public ResponseEntity<TokenResponseDTO> register(@RequestBody RegisterUserDTO registerUserDto) {
         TokenResponseDTO registeredUser = authenticationService.signup(registerUserDto);
@@ -31,6 +50,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(registeredUser);
     }
 
+
+    @Operation(
+            summary = "Authentification d'un utilisateur",
+            description = "Authentifie un utilisateur avec les informations fournies."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TokenResponseDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "401", content = { @Content(schema = @Schema()) }),
+    })
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDTO> authenticate(@RequestBody LoginUserDTO loginUserDto) {
         TokenResponseDTO authenticatedUser = authenticationService.authenticate(loginUserDto);
@@ -38,6 +66,20 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticatedUser);
     }
 
+
+    @Operation(
+            summary = "Informations de l'utilisateur authentifié",
+            description = "Récupère les informations de l'utilisateur authentifié.",
+            security = { @SecurityRequirement(name = "bearerAuth")}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = MeResponseDTO.class), mediaType = "application/json")}),
+    })
+    @Parameters({
+            @Parameter(
+                    name = "Authorization", description = "Bearer Token", required = true
+            )
+    })
     @GetMapping("/me")
     public ResponseEntity<MeResponseDTO> authenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
